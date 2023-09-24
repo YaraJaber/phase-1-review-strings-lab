542178bdbf5e772829b2b4c2819c9e092acde7b9
YaraJaber <145011720+YaraJaber@users.noreply.github.com>
2023-09-24 16:13:54 +0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = 'Welcome to Flatbook, ${currenUser}! ';
+const welcomeMessage = currentUser;
+const welcomeMessage = 'welcome to Flatbook, currentUser';
+const currentUser = 'Grace Hopper';
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
